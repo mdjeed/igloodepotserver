90f7c4f2e10377e96efcaeda78ed13e7d9bafcb9
madjed <97170344+mdjeed@users.noreply.github.com>
2026-03-14 18:30:42 +0100

    Add get_branch action to retrieve branch list

--- server.py
+++ server.py
@@ -215,7 +215,13 @@ async def handle_connection(websocket):
                 await websocket.send(json.dumps(response))
                  
                 
-
+            elif data['action'] == 'get_branch':
+                 cursor.execute("SELECT id, branchname FROM Branches")  
+                 items = cursor.fetchall()
+              
+                items_list = [{'id': item[0], 'name': item[1]} for item in items]  
+                await websocket.send(json.dumps({"status": "branch_list", "branches": items_list}))
+                
 
 
             
