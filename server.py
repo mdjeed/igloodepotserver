62d2045cd3138d0bb660c79cf4df479ebaa20125
madjed <97170344+mdjeed@users.noreply.github.com>
2026-03-15 15:28:59 +0100

    Change key in branch list from 'name' to 'branchname'

--- server.py
+++ server.py
@@ -219,7 +219,7 @@ async def handle_connection(websocket):
                  cursor.execute("SELECT id, branchname FROM Branches")  
                  items = cursor.fetchall()
               
-                 items_list = [{'id': item[0], 'name': item[1]} for item in items]  
+                 items_list = [{'id': item[0], 'branchname': item[1]} for item in items]  
                  await websocket.send(json.dumps({"status": "branch_list", "branches": items_list}))
                     
 
